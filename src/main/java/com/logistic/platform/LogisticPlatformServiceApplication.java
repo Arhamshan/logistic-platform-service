5d820e5e4c4fd64c47111b05900a6994d1420a60
Arham Muhammadh <arhamshan625@gmail.com>
2026-05-16 13:31:10 +0530

    implemented to view logs in admin client view

--- src/main/java/com/logistic/platform/LogisticPlatformServiceApplication.java
+++ src/main/java/com/logistic/platform/LogisticPlatformServiceApplication.java
@@ -1,9 +1,11 @@
 package com.logistic.platform;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class LogisticPlatformServiceApplication {
 
 	public static void main(String[] args) {
