a7256a5cdeb131ef6a9171b763c3001bfa13b2ea
Rizquan Mohamed <mohammedrizquan10@gmail.com>
2026-07-13 16:09:00 +0530

    address update in user

--- src/main/java/com/logistic/platform/dto/user/GetUserResponseDto.java
+++ src/main/java/com/logistic/platform/dto/user/GetUserResponseDto.java
@@ -13,12 +13,20 @@ public class GetUserResponseDto {
     private String username;
     private String name;
     private String contactNo;
-    private String address;
+    private String address1;
+    private String address2;
+    private String suburb;
+    private String state;
+    private String country;
 
     public GetUserResponseDto(User user) {
         this.username  = user.getUsername();
         this.name      = user.getContact() != null ? user.getContact().getName()  : null;
         this.contactNo = user.getContact() != null ? user.getContact().getPhone() : null;
-        this.address   = user.getContact() != null ? user.getContact().getAddressLine1() : null;
+        this.address1   = user.getContact() != null ? user.getContact().getAddressLine1() : null;
+        this.address2   = user.getContact() != null ? user.getContact().getAddressLine2() : null;
+        this.suburb    = user.getContact() != null ? user.getContact().getSuburb() : null;
+        this.state     = user.getContact() != null ? user.getContact().getState() : null;
+        this.country   = user.getContact() != null ? user.getContact().getCountry() : null;
     }
 }
